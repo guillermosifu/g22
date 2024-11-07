c04fb121b01ab8e2780e41d276ff5f8f32a8a224
guillermosifu <sifuentes1201@gmail.com>
2024-11-06 23:00:53 -0500

    js

---
+++ semana5/dia1/js/algoritmos.js
@@ -0,0 +1,37 @@
+// variables = contenedor , un valor de diferente tipo de dato
+// tipos de datos 
+// string = texto 
+//number = numerico
+//boll=  logico (true-false)
+// null  =  nulo
+//undefined = no esta definido 
+//symbol 
+//objetc
+
+
+// varibales 3 formas de declarar  VAR -LET CONST
+
+//let = cambiar .variar  alumnos= "lesly"
+
+// const =  PI=314.36666
+
+//calculadora
+
+// alert("hola mundo 22")
+
+
+
+let numero1 ="100"
+let numero2 = ("ingresa el valor")
+
+resultado = numero1 + numero2
+
+
+
+console.log(resultado)
+console.warn("advertencia")
+
+console.error("error")
+
+
+
