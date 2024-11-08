f89c0ba599a287fabb23c31794a1d8ef9a79e017
guillermosifu <sifuentes1201@gmail.com>
2024-11-07 20:17:53 -0500

    array

---
+++ semana5/dia2/miprimerarchivo.js
@@ -0,0 +1,39 @@
+//tipo de datos
+//numericos 
+
+const numeroPar= 2;
+let numeroImpar=1;
+
+// string -textos 
+
+const nombre = "guillermo sifuentes montoya";
+
+// booleano  true -false (1-0)
+
+const esVerdad= true;
+const esFalso=false
+
+//nulos =indefinidos
+
+let nulo = null
+let valorIndefinido 
+
+// array arreglos 
+
+const nombres=["guillermo","juan","pepe"] 
+const datos=["guillermo",35,true,20]
+
+
+datos[0]="pepe"
+datos[5]=false
+
+console.log(datos)
+
+//ejercicio JS 
+// van a realizar 2 array con la siguientes informacion
+// primer lo que tiene en su cocina 
+// nombres de las ex o exs
+
+// cambiar del primero el elemetno 4 
+//de segundo van insertar 2 elemetos al final 
+
